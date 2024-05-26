bf723d36abf98cde968aa90743bf8c1156687d3c
Eric Silva <ericdss@hotmail.com>
2024-05-26 19:48:51 -0300

    fix: ajustar mapeamento ao obter produtos

--- src/Modules/Pedido/ControlePedidos.Pedido.Infrastructure/Repositories/Http/DTO/ProdutoApiDto.cs
+++ src/Modules/Pedido/ControlePedidos.Pedido.Infrastructure/Repositories/Http/DTO/ProdutoApiDto.cs
@@ -7,7 +7,7 @@ public class ProdutoApiDto
     public string Id { get; set; }
     public DateTime DataCriacao { get; set; }
     public string Nome { get; set; }
-    public Dictionary<string, decimal> TamanhoPreco { get; set; }
+    public List<KeyValuePair<string, decimal>> TamanhoPreco { get; set; }
     public string TipoProduto { get; set; }
     public string Descricao { get; set; }
     public bool Ativo { get; set; }
@@ -18,7 +18,7 @@ public class ProdutoApiDto
 
         return new Domain.Entities.Produto(model.Id,
                                            model.Nome,
-                                           model.TamanhoPreco,
+                                           model.TamanhoPreco.ToDictionary(),
                                            model.TipoProduto,
                                            model.Descricao,
                                            model.DataCriacao,
