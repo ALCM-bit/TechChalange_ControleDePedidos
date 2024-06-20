d5e25f3e5a88c32ee362382d8b46f3197d1b0125
Felipe Augusto L. C. Magalhães <falcm-bit@outlook.com>
2024-06-20 20:43:47 -0300

    Inicio da criação do UseCase de Produto - Obter Único

---
+++ src/Modules/Produto/CadastroPedidos.Produto.Application/UseCases/ObterProduto/ObterProdutoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPedidos.Produto.Application.UseCases.ObterProduto;
+
+public class ObterProdutoRequest
+{
+    public string IdProduto { get; set; }
+}
